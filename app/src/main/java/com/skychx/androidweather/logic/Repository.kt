6b9c2d5eef3733106d8b7b134e80a1694b241e8b
skychx <skychx@hotmail.com>
2022-02-07 22:39:21 +0800

    feat: update network code

--- app/src/main/java/com/skychx/androidweather/logic/Repository.kt
+++ app/src/main/java/com/skychx/androidweather/logic/Repository.kt
@@ -2,22 +2,58 @@ package com.skychx.androidweather.logic
 
 import androidx.lifecycle.liveData
 import com.skychx.androidweather.logic.model.Place
+import com.skychx.androidweather.logic.model.Weather
 import com.skychx.androidweather.logic.network.WeatherNetwork
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
+import kotlinx.coroutines.coroutineScope
+import kotlin.coroutines.CoroutineContext
 
 object Repository {
-    fun searchPlaces(query: String) = liveData(Dispatchers.IO) {
-        val result = try {
-            val placeResponse = WeatherNetwork.searchPlaces(query)
-            if (placeResponse.status == "ok") {
-                val places = placeResponse.places
-                Result.success(places)
-            } else {
-                Result.failure(RuntimeException("response status is ${placeResponse.status}"))
-            }
-        } catch (e: Exception) {
-            Result.failure<List<Place>>(e)
+    fun searchPlaces(query: String) = fire(Dispatchers.IO) {
+        val placeResponse = WeatherNetwork.searchPlaces(query)
+        if (placeResponse.status == "ok") {
+            val places = placeResponse.places
+            Result.success(places)
+        } else {
+            Result.failure(RuntimeException("response status is ${placeResponse.status}"))
         }
-        emit(result)
     }
+
+    fun refreshWeather(lng: String, lat: String, placeName: String) = fire(Dispatchers.IO) {
+        coroutineScope {
+            val deferredRealtime = async {
+                WeatherNetwork.getRealtimeWeather(lng, lat)
+            }
+            val deferredDaily = async {
+                WeatherNetwork.getDailyWeather(lng, lat)
+            }
+
+            // 实现并发
+            val realtimeResponse = deferredRealtime.await()
+            val dailyResponse = deferredDaily.await()
+
+            if (realtimeResponse.status == "ok" && dailyResponse.status == "ok") {
+                val weather = Weather(realtimeResponse.result.realtime, dailyResponse.result.daily)
+                Result.success(weather)
+            } else {
+                Result.failure(
+                    RuntimeException(
+                        "realtime response status is ${realtimeResponse.status}" +
+                                "daily response status is ${dailyResponse.status}"
+                    )
+                )
+            }
+        }
+    }
+
+    private fun <T> fire(context: CoroutineContext, block: suspend () -> Result<T>) =
+        liveData<Result<T>>(context) {
+            val result = try {
+                block()
+            } catch (e: Exception) {
+                Result.failure<T>(e)
+            }
+            emit(result)
+        }
 }
---
+++ app/src/main/java/com/skychx/androidweather/logic/network/WeatherService.kt
@@ -0,0 +1,16 @@
+package com.skychx.androidweather.logic.network
+
+import com.skychx.androidweather.WeatherApplication
+import com.skychx.androidweather.logic.model.DailyResponse
+import com.skychx.androidweather.logic.model.RealtimeResponse
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+interface WeatherService {
+    @GET("v2.5/${WeatherApplication.TOKEN}/{lng},{lat}/realtime.json")
+    fun getRealtimeWeather(@Path("lng") lng: String, @Path("lat") lat: String): Call<RealtimeResponse>
+
+    @GET("v2.5/${WeatherApplication.TOKEN}/{lng},{lat}/daily.json")
+    fun getDailyWeather(@Path("lng") lng: String, @Path("lat") lat: String): Call<DailyResponse>
+}
